d77649e5ed369b61e3641fa2623203d6a6aeb3ce
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-23 22:14:09 +0530

    created repo item component

--- migration/src/components/content/Content.js
+++ migration/src/components/content/Content.js
@@ -2,6 +2,15 @@ import React from 'react'
 import './Content.css'
 
 export default function Content() {
+
+  const content_repo_data = [
+    1,2,3,4,5
+  ]
+
+  const content_repo = content_repo_data.map(item => {
+    return <Content></Content>
+  })
+
   return (
     <div className="content">
         <div className="content-section">
@@ -11,7 +20,6 @@ export default function Content() {
                 <div className="content-section-grid-item"></div>
                 <div className="content-section-grid-item"></div>
                 <div className="content-section-grid-item"></div>
-
             </div>
         </div>
     </div>
---
+++ migration/src/components/content/RepoItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RepoItem(props) {
+  return (
+    <div className="content-section-grid-item"></div>
+  )
+}
