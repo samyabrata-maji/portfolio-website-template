54a81730ee4e332b9a11c5fdc2505ceccc6c3756
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-28 19:35:39 +0530

    reformatted code

--- src/components/content/RepoItem.js
+++ src/components/content/RepoItem.js
@@ -4,23 +4,6 @@ import "./RepoItem.css"
 
 export default function RepoItem(props) {
   
-  // let [icons_group, setIconsGroup] = useState(_ => [])
-  // useEffect(() => {
-  //   // console.log(props.data.name)
-  //   setIconsGroup(_ => {
-  //     let icon_names = []
-  //     if (props !== undefined && props !== null && props.data.name !== "") {
-  //       fetchLanguages(props.data.name).then(data => {
-        
-  //       })
-  //     }
-  //     console.log("hello")
-  //   })
-  // }, [props.data.name])
-  
-
-  // if ()
-
   return (
     <div className="content-section-repo-item">
         <h3>{props.data.name}</h3>
