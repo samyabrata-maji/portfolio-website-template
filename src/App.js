17ef42eb7c12621ac6c6e1bd231f85e5f281a14c
Samya brata Maji <samyabratamaji334@gmail.com>
2022-11-29 17:18:04 +0530

    working on routing #10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./components/content_home/Home";
 import ContentRepo from "./components/content_repo/ContentRepo";
+import Tech from "./components/content_tech/Tech";
 import SideBar from "./components/sidebar/SideBar";
 
 
@@ -8,7 +10,9 @@ function App() {
     <>
       <SideBar></SideBar>
       <Routes>
-        <Route path="/" element={<ContentRepo/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/repo" element={<ContentRepo/>}></Route>
+        <Route path="/tech" element={<Tech/>}></Route>
       </Routes>
     </>
   );
---
+++ src/components/content_tech/Tech.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Tech() {
+  return (
+    <div>
+      <h2>Tech</h2>
+    </div>
+  )
+}
